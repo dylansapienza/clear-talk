5d70073e4e10f4230cf94902b084730b659bfa9c
Dylan Sapienza <dylsapienza12@gmail.com>
2021-02-12 12:40:05 -0500

    Setup Homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,40 @@
-
 import React, { Component } from 'react';
+import {IonApp} from "@ionic/react";
+import { BrowserRouter, Route } from "react-router-dom";
+import "@ionic/core/css/ionic.bundle.css";
 import './App.css';
-import axios from 'axios'
+import Home from "./components/Home";
+import axios from 'axios';
 
-class App extends Component {
-  state = {
-    response: {}
-  };
+// class App extends Component {
+//   state = {
+//     response: {}
+//   };
   
-  componentDidMount() {
-    axios.get('/api/v1/say-something').then((res) => {
-      const response = res.data;
-      this.setState({response});
-    });
-  }
+//   componentDidMount() {
+//     axios.get('/api/v1/say-something').then((res) => {
+//       const response = res.data;
+//       this.setState({response});
+//     });
+//   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello from the frontend!</h1>
-        <h1>{this.state.response.body}</h1>
-      </div>
-    );
-  }
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hello from the frontend!</h1>
+//         <h1>{this.state.response.body}</h1>
+//       </div>
+//     );
+//   }
+// }
+function App() {
+  return (
+    <BrowserRouter>
+      <IonApp>
+        <Route exact path="/" component={Home} />
+    </IonApp>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/components/Home.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from "react";
+import {
+  IonApp,
+  IonHeader,
+  IonTitle,
+  IonCard,
+  IonToolbar,
+  IonCardContent,
+  IonCardTitle,
+  IonPage,
+  IonContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonButton,
+  IonText,
+  IonLabel,
+  IonRow,
+} from "@ionic/react";
+import "@ionic/core/css/ionic.bundle.css";
+
+function Home(props) {
+  const [error, setError] = useState("");
+
+  function getError() {
+    const query = new URLSearchParams(props.location.search);
+    const error = query.get("error");
+    if (error !== "") {
+      setError(error);
+    }
+  }
+
+  useEffect(() => {
+    getError();
+  }, []);
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>
+            Clear Talk
+            </IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle class="ion-text-center"><div class="typewriter">
+                <h1>Clear Talk</h1>
+            </div></IonCardTitle>
+          </IonCardHeader>
+
+          <IonCardContent class="ion-text-center ion-text-bold">
+            <IonText color="dark">
+              Clear Talk is a small web-based chat app for personal communications.
+            </IonText>
+            <hr></hr>
+            <IonText color="danger">
+              <i>{error}</i>
+            </IonText>
+            <IonButton
+              href="/signup"
+              expand="block"
+              color="success"
+              strong="true"
+            >
+              Sign Up
+            </IonButton>
+            <hr></hr>
+            <hr></hr>
+            <hr></hr>
+            <IonText color="dark">
+              <h2>Already have an account?</h2>
+            </IonText>
+            <hr></hr>
+            <IonButton
+              href="/login"
+              expand="block"
+              color="primary"
+              strong="true"
+            >
+              Login
+            </IonButton>
+          </IonCardContent>
+        </IonCard>
+      </IonContent>
+    </IonPage>
+  );
+}
+export default Home;
